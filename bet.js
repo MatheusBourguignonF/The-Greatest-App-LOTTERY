29e29f1994be8585c1393cd9c00895642db24a46
MatheusBourguignonF <teusaoboladao@gmail.com>
2026-02-23 18:59:27 -0300

    Atualização de JavaScript

--- bet.js
+++ bet.js
@@ -1,5 +1,7 @@
 let gameTypes = [] 
 let selectedButton = null 
+let selectedNumbers = []
+let currentType = null
 
 async function getAllGames() { 
     try{ 
@@ -14,14 +16,43 @@ async function getAllGames() {
             throw new Error('Erro ao carregar o arquivo JSON')
             }  
         gameTypes = data.types
+        createButtons(gameTypes)
     }
  catch (error) {
     console.error('Erro:', error.message)
   }
-createButtons(gameTypes)
-}
+}   
 getAllGames()
 
+function createDescription(type) {
+    const descriptionContainer = document.querySelector('.div_description2')
+
+    descriptionContainer.innerHTML = ''
+
+    const description = document.createElement('p')
+    description.innerText = type.description
+
+    descriptionContainer.appendChild(description)
+    description.className = 'p_description'
+}
+
+function createNumbers(type) {
+    const numbersContainer = document.querySelector('.Number_Buttons')
+
+    numbersContainer.innerHTML = ''  
+
+    Array.from({length: type.range}).map((_,number) =>{
+        const button = document.createElement('button')
+        button.innerText = number + 1   
+        button.className = 'button_bet'
+
+        button.addEventListener('click', () => {
+            choseNumbers(button)
+        })
+        numbersContainer.appendChild(button)
+    }
+    )
+}
 function click(button, type) {
 
     if (selectedButton) {
@@ -32,11 +63,12 @@ function click(button, type) {
     button.style.color = 'white'
 
     selectedButton = button
+    currentType = type
+    selectedNumbers = [] // limpa seleção ao trocar jogo
 
     createDescription(type)
     createNumbers(type)
 }
-
 function createButtons(types) { 
 
     const buttonsContainer = document.querySelector('.buttons1')
@@ -64,6 +96,7 @@ function createButtons(types) {
             button.style.background = type.color
             button.style.color = 'white'
             selectedButton = button
+            currentType = type
             createDescription(type)
             createNumbers(type)
         }
@@ -74,32 +107,26 @@ function createButtons(types) {
     })
 }
 
-function createDescription(type) {
-    const description = document.createElement('description')
-    const descriptionContainer = document.querySelector('.div_description2')
+function choseNumbers(button) {
 
-    descriptionContainer.innerHTML = ''
+    const number = Number(button.innerText) // Conversão de string para número
+    const max = currentType["max-number"] // Define o limite de números que podem ser selecionados para o jogo atual.
 
-    const p = document.createElement('p')
-    p.innerText = type.description
-
-    descriptionContainer.appendChild(p)
-    p.className = 'p_description'
-}
-
-function createNumbers(type) {
-    const numbersContainer = document.querySelector('.Number_Buttons')
-
-    numbersContainer.innerHTML = ''  
-
-    Array.from({length: type.range}).map((index,number) =>{
-        const button = document.createElement('button')
-        button.innerText = number + 1
-        button.className = 'button_bet'
-
-        numbersContainer.appendChild(button)
-        
+    const index = selectedNumbers.indexOf(number) // Procura um valor dentro da lista
+    // Se já está selecionado → desmarca
+    if (index !== -1) {
+        selectedNumbers.splice(index, 1) //Remova 1 elemento a partir da posição index
+        button.style.background = ''
+        button.style.color = ''
+        return
     }
-    )
+    // Se atingiu limite
+    if (selectedNumbers.length >= max) { 
+        alert(`Você só pode selecionar ${max} números em ${currentType.type}`) 
+        return // Impede que o número seja selecionado além do limite.
+    }
+    // Marca número
+    selectedNumbers.push(number) //Adiciona número como selecionado
+    button.style.background = currentType.color
+    button.style.color = 'white'
 }
-
